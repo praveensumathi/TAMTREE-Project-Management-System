08305f72758b99c4739b1a6421a56cdaaaa2e04a
Logeswaran <logeswaran2108@gmail.com>
2023-12-05 17:29:32 +0530

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { useState } from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import Layout from "./layout/Layout";
+import Projects from "./pages/projects/Projects";
+import Board from "./pages/board/Board";
+
+
+
 
 function App() {
-  return <>Project Management</>;
+  return (
+    <>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Projects />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/boards" element={<Board />} />
+            
+          </Route>
+        </Routes>
+        </Router>
+      
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/projects/Projects.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material"
+
+function Projects() {
+  return (
+    <Box>Projects</Box>
+  )
+}
+
+export default Projects
