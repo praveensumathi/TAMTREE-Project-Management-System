677d90527333eeee6c68f19c39104385e1055040
praveenR <praveen.r@deventure.co>
2023-12-05 11:35:43 +0530

    added basci folder structre

---
+++ src/seed-data/seed-data.ts
@@ -0,0 +1,14 @@
+import { Project } from "../types/type";
+
+export const projects: Project[] = [
+  {
+    title: "Project 1",
+    description: "Project Description",
+    _id: "p1",
+  },
+  {
+    title: "Project 2",
+    description: "Project Description",
+    _id: "p2",
+  },
+];
